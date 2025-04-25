b52eb6d723f08b046a077aeff1e78292b689bc7a
cyoo28 <charlieyoo00@gmail.com>
2025-04-25 15:11:01 -0400

    IX-80: Added optional limit when listing keys and added function to create keys

--- api_key_rotation.py
+++ api_key_rotation.py
@@ -10,46 +10,53 @@ class GCP:
     def custom_exec(self, command):
         response = os.popen(command)
         return json.loads(response.read())
-        
+    
 class SecretManager:
-    def __init__(self, projectID):
+    def __init__(self, projectID, credManager):
         self.projectID = projectID
         self.GCP = GCP(self.projectID)
-        self.KeyGen = KeyManager(self.projectID, self.GCP)
+        self.credManager = credManager
     def list_secrets(self):
-        secrets = self.GCP.exec("secrets list", "json")
+        secrets = self.GCP.read_exec("secrets list", "json")
         return secrets
     def describe_secret(self, secretName):
-        secret = self.GCP.exec("secrets describe {}".format(secretName), "json")
+        secret = self.GCP.read_exec("secrets describe {}".format(secretName), "json")
         return secret
     def list_versions(self, secretName):
-        versions = self.GCP.exec("secrets versions list {}".format(secretName, "json"))
+        versions = self.GCP.read_exec("secrets versions list {}".format(secretName, "json"))
         return versions
     def disable_version(self, version, secretName):
         self.GCP.write_exec("secrets versions disable {} --secret={}".format(version, secretName))
     def add_version(self, keyName, secretValue, secretName):
         self.GCP.custom_exec("echo -n {} | gcloud secrets versions add {} --data-file=- --project={}".format(secretValue, secretName, self.projectID))
-        version = self.GCP.exec("secrets versions list {} --limit=1 --sort-by=~createTime".format(secretName), "value(name)")
+        version = self.GCP.read_exec("secrets versions list {} --limit=1 --sort-by=~createTime".format(secretName), "value(name)")
         annotation = "version_{}={}".format(version, keyName.replace(" ","_"))
         self.GCP.write_exec("secrets update {} --update-annotations={}".format(secretName, annotation))
         # should i save key name or ID in annotation? leaning towards ID
+
+
 class KeyManager:
-    def __init__(self, projectID, GCP):
+    def __init__(self, projectID):
         self.projectID = projectID
-        self.GCP = GCP
-    def list_keys(self):
-        keys = self.GCP.exec("services api-keys list")
+        self.GCP = GCP(self.projectID)
+    def list_keys(self, limit=None):
+        if not limit:
+            keys = self.GCP.exec("services api-keys list --sort-by=~createTime")
+        else:
+            keys = self.GCP.exec("services api-keys list --limit={} --sort-by=~createTime".format(limit))
         return keys
     def delete_key(self, keyID):
         self.GCP.exec("services api-keys delete {}".format(keyID))
-    
+    def create_key(self, keyName):
+        response = self.GCP.write_exec("services api-keys create --display-name='{}'".format(keyName))
+        keyID = response['response']['uid']
+        return keyID
+
     # how to clone a key?
     def get_key_config(self, keyID):
         self.keyConfig = self.GCP.exec("services api-keys describe {}".format(keyID))
         return self.keyConfig
-    # How to get Key ID when creating a key
-    def create_key(self, keyName):
-        self.GCP.exec("services api-keys create --display-name={}".format(keyName))
+    
     def update_key(self, keyName):
         self.GCP.exec("services api-keys update --display-name={} --restrictions={}".format(keyName, json.dumps(self.keyConfig)))
     

10a97defdcc40107491cced3c8bb16f9ab9a95d6
cyoo28 <charlieyoo00@gmail.com>
2025-05-01 14:42:48 -0400

    IX-80: Added script to check that secrets have only the latest version enabled

--- secret_config_check.py
+++ secret_config_check.py
@@ -208,40 +208,50 @@ class KeyManager:
         self.delete_key(oldKeyId)
         return newKeyId, newKeyString
 
-def main(projectId, expiryTime, fileName="secrets-rotation.csv", debug=False, test=False):
+def main(projectId, fileName="secrets-config.csv", debug=False, test=False):
     kMan = KeyManager(projectId, debug, test)
     sMan = SecretManager(projectId, kMan, debug, test)
-    sMan.rotate_secrets(expiryTime)
     with open(fileName, "w") as file:
-        file.write("Secret Name, Old Secret Version, Old Key Id, New Secret Version, New Key Id, Error\n")
-    for secretName, secretInfo in sMan.rotatedSecrets.items():
-        if isinstance(secretInfo, str):
+        file.write("Secret Name, Status, Latest Version Enabled, Versions Enabled, Error\n")
+    secrets = sMan.list_secrets()
+    if not secrets:
+        print("Error: There are no secrets in this project")
+    for secret in secrets:
+        secretName = secret.get("name").split("/")[-1]
+        versions = sMan.list_versions(secretName)
+        if not versions:
+            print(f"Error: {secretName} has no versions")
             with open(fileName, "a") as file:
-                file.write(f"{secretName}, -, -, -, -, {secretInfo}\n")
+                file.write(f"{secretName}, INSUFFICIENT DATA, -, -, No versions\n")
+            continue
+        totalEnabled = sum(1 for version in versions if version.get('state') == 'ENABLED')
+        latestVersion = sMan.latest_version(secretName, enabled=False)
+        latestEnabled = latestVersion.get('state')=='ENABLED'
+        print(f"{secretName}:\n  is latest enabled: {latestEnabled}\n  total versions enabled: {totalEnabled}")
+        with open(fileName, "a") as file:
+            file.write(f"{secretName}")
+        if latestEnabled and totalEnabled <= 1:
+            with open(fileName, "a") as file:
+                file.write(", OK")
         else:
             with open(fileName, "a") as file:
-                file.write(f"{secretName}")
-            for secretVersion, keyId in secretInfo.items():
-                with open(fileName, "a") as file:
-                    file.write(f", {secretVersion}, {keyId}")
-            with open(fileName, "a") as file:
-                    file.write(", -\n")
+                file.write(", IN VIOLATION")     
+        with open(fileName, "a") as file:
+            file.write(f", {latestEnabled}, {totalEnabled}, -\n")
 
 if __name__ == "__main__":
     # Create an ArgumentParser object
     parser = argparse.ArgumentParser(description="Use this script to rotate keys associated with old secrets")
     # Create arguments
     parser.add_argument("projectId", type=str, help="Google Cloud Project Id")
-    parser.add_argument("expiryTime", type=int, help="Time in days after which secrets should be rotated")
-    parser.add_argument("--fileName", dest="fileName", type=str, default="secrets-rotation.csv", help="Name of your file (\"secrets-rotation.csv\" if not specified)")
+    parser.add_argument("--fileName", dest="fileName", type=str, default="secrets-config.csv", help="Name of your file (\"secrets-config.csv\" if not specified)")
     parser.add_argument("--debug", dest="debug", action="store_true", help="Enable debug mode")
     parser.add_argument("--test", dest="test", action="store_true", help="Enable dry-run testing mode")
     # Parse the command-line arguments
     args = parser.parse_args(sys.argv[1:])
     projectId = args.projectId
-    expiryTime = args.expiryTime
     fileName = args.fileName
     debug = args.debug
     test = args.test
     # Pass arguments to the main function
-    main(projectId, expiryTime, fileName, debug, test)
+    main(projectId, fileName, debug, test)
